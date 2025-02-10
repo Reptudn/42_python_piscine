8695292cc92aee702cab7bea1f1795159d4bc59c
jkauker <jkauker@student.42heilbronn.de>
2025-02-10 16:26:32 +0100

    flakeed ex08 progress bar

---
+++ Python00/ex08/Loading.py
@@ -0,0 +1,35 @@
+import time
+import shutil
+
+
+def format_time(seconds: float) -> str:
+    mins, secs = divmod(seconds, 60)
+    return f"{int(mins):02}:{int(secs):02}"
+
+
+def ft_tqdm(lst: range) -> None:  # type: ignore
+    width = shutil.get_terminal_size().columns
+    bar_width = width - 40
+    start = time.time()
+
+    curr = -1
+    total = len(lst)
+    print("\r", end="", flush=True)
+    for item in lst:
+        curr += 1
+        time_elapsed = time.time() - start
+        percentage = (curr + 1) / total
+        bar_length = int(bar_width * percentage)
+        progbar = f"{'█' * bar_length}{' ' * (bar_width - bar_length)}"
+        itpersec = (curr + 1) / time_elapsed
+        est_total_time = time_elapsed / percentage
+        est_remaining_time = est_total_time - time_elapsed
+        percentage_str = f"{percentage:.0%}".rjust(4)
+        elapsed_str = format_time(time_elapsed)
+        remaining_str = format_time(est_remaining_time)
+        print(
+            f"\r{percentage_str}|{progbar}| {curr + 1}/{total} "
+            f"[{elapsed_str}<{remaining_str}, {itpersec:.2f}it/s]",
+            end="", flush=True
+        )
+        yield item
